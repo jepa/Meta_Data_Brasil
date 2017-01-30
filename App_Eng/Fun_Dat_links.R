3fd505aa4d612f5198a54a940d59636ae84526e6
jepa <jpalacios@bren.ucsb.edu>
2017-01-30 11:34:29 -0800

    App in Eng

---
+++ App_Eng/Fun_Dat_links.R
@@ -0,0 +1,16 @@
+
+#This function converts the reference category of the metadataset template into actual "clickable" links.
+
+#Variables: 
+#Link, the actual urll "www.somewhere.com"
+#Reference, the name that will be displayed in the Dataset
+
+Ref_Links <- function(Link,Reference){
+ 
+  Result = paste("<a href=",Link,">",Reference,"</a>") 
+  Result = data.frame(Result)  
+  colnames(Result) <- "Link"
+  
+  return(Result)
+  }
+  
