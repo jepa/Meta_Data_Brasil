78dcf7f1a23dd8a6722acfd0c8e4aab8b215ef28
jepa <jpalacios@bren.ucsb.edu>
2017-02-12 17:22:42 -0800

    Updated both versions of the app. Fixed the results section. Added the date of the counting. Both languages.

--- App_Esp/server.R
+++ App_Esp/server.R
@@ -347,7 +347,26 @@ shinyServer(function(input, output, session) {
   })
   
   
-  #### Collaboration ###
+  #### Collaboration ####
+  
+  #### Template Download ####
+  
+  TempInput <- reactive({
+    
+    data<- fread("./Data_Download/Metadata_Template.csv")
+    data.frame(data)
+  })
+  
+  output$downloadTemp <- downloadHandler(
+    filename = function() { 
+      paste(input$dataset, 'MIM_Template',".csv", sep='') 
+    },
+    content = function(file) {
+      write.csv(TempInput(), file)
+    }
+  )
+  
+  #### Colaborating ####
   output$Institutions <- renderDataTable({
     
     Inst_Table <- datasetInput() %>% 
