6a209100a25c106b6241efdbec9bbe7f61963f6e
jepa <jpalacios@bren.ucsb.edu>
2018-03-06 11:17:50 -0800

    Updated server

--- App/server.R
+++ App/server.R
@@ -52,7 +52,7 @@ shinyServer(function(input, output) {
   # Reading the Template ####
   datasetInput <- reactive({
     
-    data<- fread("~/Documents/Dropbox/Metadata_Mexico/English/Templates/Template_6.4.csv",
+    data<- fread("~/Documents/Dropbox/Metadata_Mexico/English/Templates/Template_6.8.csv",
                  colClasses = c(Area = 'character',
                                 Notes = 'character',
                                 # Data_Uncertanty ='character',
